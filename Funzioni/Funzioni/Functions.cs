6073efa9ebf90d32f06fad55be00100c67b1753e
Riccardo Canal <canal.riccardo04@gmail.com>
2024-12-31 20:44:54 +0100

    Esercizio 1
    Dichiarare una funzione che restituisce un valore di tipo int e accetta due parametri.

--- Funzioni/Funzioni/Functions.cs
+++ Funzioni/Funzioni/Functions.cs
@@ -2,6 +2,10 @@
 {
     internal class Functions
     {
-        
+        // Esercizio 1: Dichiarare una funzione che restituisce un valore di tipo int e accetta due parametri.
+        internal static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
--- Funzioni/Funzioni/Program.cs
+++ Funzioni/Funzioni/Program.cs
@@ -8,6 +8,11 @@
             Console.WriteLine("======================");
             Console.WriteLine("Exercises on Functions");
             Console.WriteLine("======================");
+
+            // Esercizio 1: Dichiarare una funzione che restituisce un valore di tipo int e accetta due parametri.
+            Console.WriteLine("\nExercise 1:\n");
+            int sumResult = Functions.AddNumbers(10, 5);
+            Console.WriteLine($"Sum: {sumResult}");
         }
     }
 }
