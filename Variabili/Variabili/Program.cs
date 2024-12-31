98f29ffbc1fbc926317afa95d6a6127a9d5961fd
Riccardo Canal <canal.riccardo04@gmail.com>
2024-12-31 18:30:47 +0100

    Esercizio 1
    Dichiarare una variabile di tipo int e assegnarle un valore, quindi modificarlo.

--- Variabili/Variabili/Program.cs
+++ Variabili/Variabili/Program.cs
@@ -8,6 +8,13 @@
             Console.WriteLine("======================");
             Console.WriteLine("Exercises on Variables");
             Console.WriteLine("======================");
+
+            // Esercizio 1: Dichiarare una variabile di tipo int e assegnarle un valore, quindi modificarlo.
+            Console.WriteLine("\nExcercise 1:\n");
+            int number = 10;
+            Console.WriteLine("Initial value of number: " + number);
+            number = 20;
+            Console.WriteLine("Modified value of number: " + number);
         }
     }
 }
